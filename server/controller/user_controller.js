2a38b10a09a0b547859fc979afab148257fdda90
luffan <mihalkovgleb@gmail.com>
2022-05-27 03:51:32 +0300

    feat: add payment and tax controllers

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -1,44 +1,22 @@
-const db = require('../db')
-
+const db = require("../db");
 
 class UserController {
-    async createUser(req, res) {
-        const {name, surname, middlename, passport_id, login, password, district, income, expenses} = req.body
+  async createUser(req, res) {
+    const {
+      name,
+      surname,
+      middlename,
+      passport_id,
+      login,
+      password,
+      district,
+      income,
+      expenses,
+    } = req.body;
 
-        const newUser = await db.query(
-          "INSERT INTO client (name, surname, middlename, passport_id, login, password, district, income, expenses) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
-          [
-            name,
-            surname,
-            middlename,
-            passport_id,
-            login,
-            password,
-            district,
-            income,
-            expenses,
-          ]
-        );
-
-        res.json(newPerson.rows[0])
-    }
-
-    async getUsers(req, res) {
-        const users = await db.query('SELECT * FROM client')
-        res.json(users.rows)
-    }
-
-    async getOneUser(req, res) {
-        const id = req.params.id
-
-        const user = await db.query("SELECT * FROM client where id = $1", [id]);
-
-        res.json(user.rows[0])
-    }
-
-    async updateUser(req, res) {
-      const {
-        id,
+    const newUser = await db.query(
+      "INSERT INTO client (name, surname, middlename, passport_id, login, password, district, income, expenses) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      [
         name,
         surname,
         middlename,
@@ -48,33 +26,63 @@ class UserController {
         district,
         income,
         expenses,
-      } = req.body;
+      ]
+    );
 
-      const user = await db.query(
-        "UPDATE client set name = $1, surname = $2, middlename = $3, passport_id = $4, login = $5, password = $6, district = $7, income = $8, expenses = $9 where id = $10 RETURNING *",
-        [
-          name,
-          surname,
-          middlename,
-          passport_id,
-          login,
-          password,
-          district,
-          income,
-          expenses,
-          id,
-        ]
-      );
-    }
+    res.json(newPerson.rows[0]);
+  }
 
-    async deleteUser(req, res) {
-        const id = req.params.id
+  async getUsers(req, res) {
+    const users = await db.query("SELECT * FROM client");
+    res.json(users.rows);
+  }
 
-        const user = await db.query("DELETE * FROM client where id = $1", [id]);
-        
-        res.json(user.rows[0])
-    }
+  async getOneUser(req, res) {
+    const id = req.params.id;
 
+    const user = await db.query("SELECT * FROM client where id = $1", [id]);
+
+    res.json(user.rows[0]);
+  }
+
+  async updateUser(req, res) {
+    const {
+      id,
+      name,
+      surname,
+      middlename,
+      passport_id,
+      login,
+      password,
+      district,
+      income,
+      expenses,
+    } = req.body;
+
+    const user = await db.query(
+      "UPDATE client set name = $1, surname = $2, middlename = $3, passport_id = $4, login = $5, password = $6, district = $7, income = $8, expenses = $9 where id = $10 RETURNING *",
+      [
+        name,
+        surname,
+        middlename,
+        passport_id,
+        login,
+        password,
+        district,
+        income,
+        expenses,
+        id,
+      ]
+    );
+  }
+
+  async deleteUser(req, res) {
+    const id = req.params.id;
+
+    const user = await db.query("DELETE * FROM client where id = $1", [id]);
+
+    res.json(user.rows[0]);
+  }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
