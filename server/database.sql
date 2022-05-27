f20f5449197bfc945d72745489531c995e4f8d72
losich.maksim <losych.maksim@gmail.com>
2022-05-27 03:37:30 +0300

    income expenses tables

--- server/database.sql
+++ server/database.sql
@@ -17,6 +17,7 @@ create TABLE tax(
     expiration_date TIMESTAMP,
     client_id INTEGER,
     payment_id INTEGER,
+    year smallint,
     FOREIGN KEY (client_id) REFERENCES client (id),
     FOREIGN KEY (payment_id) REFERENCES payment (id)
 );
@@ -25,4 +26,29 @@ create TABLE payment(
     id SERIAL PRIMARY KEY,
     date TIMESTAMP,
     amount REAL
+);
+
+create TABLE worker(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    surname VARCHAR(50),
+    middlename VARCHAR(50),
+    login VARCHAR(50),
+    password VARCHAR(50),
+);
+
+create TABLE income(
+    id SERIAL PRIMARY KEY,
+    sum REAL,
+    year smallint,
+    client_id INTEGER,
+    FOREIGN KEY (client_id) REFERENCES client (id),
+);
+
+create TABLE expenses(
+    id SERIAL PRIMARY KEY,
+    sum REAL,
+    year smallint,
+    client_id INTEGER,
+    FOREIGN KEY (client_id) REFERENCES client (id),
 );
